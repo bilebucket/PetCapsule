04c4492faf59736be94c04276a2f165d86fec1d7
sapake <sapakapi@gmail.com>
2016-04-06 19:47:22 +0200

    Tausta logiikka aloitettu

---
+++ PetCapsuleGUI/Logic/FileLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace PetCapsuleGUI.Logic
+{
+    class FileLoader
+    {
+        private string filename;
+        private List<User> users;
+
+        public List<User> Users
+        {
+            get { return users; }
+        }
+
+        public FileLoader(string filename)
+        {
+            this.filename = filename;
+        }
+
+
+        private void readUserData()
+        {
+            try
+            {
+                string fileContents = System.IO.File.ReadAllText(this.filename);
+
+                try
+                {
+                    users = JsonConvert.DeserializeObject<List<User>>(fileContents);
+                }
+                catch (JsonReaderException e) { }
+                catch (JsonSerializationException e) { }
+
+
+            }
+            catch (FileNotFoundException e) { }
+        }
+    }
+}
