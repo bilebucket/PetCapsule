11f0974fe7583c98ab166fd21e95d4dcb35127e5
Panu Partanen <k2857@student.jamk.fi>
2016-04-06 20:28:32 +0300

    Toimii

--- PetCapsuleGUI/Pages/ForgotpasswordPage.xaml.cs
+++ PetCapsuleGUI/Pages/ForgotpasswordPage.xaml.cs
@@ -20,11 +20,21 @@ namespace PetCapsuleGUI.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class JOTAIN : Page
+    public sealed partial class ForgotpasswordPage : Page
     {
-        public JOTAIN()
+        public ForgotpasswordPage()
         {
             this.InitializeComponent();
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
---
+++ PetCapsuleGUI/Pages/MyinfoPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PetCapsuleGUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MyinfoPage : Page
+    {
+        public MyinfoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FrontPage));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameBox.IsReadOnly = false;
+            LastnameBox.IsReadOnly = false;
+            EmailBox.IsReadOnly = false;
+            AddressBox.IsReadOnly = false;
+            LocationBox.IsReadOnly = false;
+        }
+    }
+}
